c7a66c48e31cb207116167d8eef3c006fded677d
Werner Detter <werner@aloah-from-hell.de>
2012-04-10 11:50:14 +0200

    modiefied comment

--- src/cmime_qp.c
+++ src/cmime_qp.c
@@ -14,10 +14,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/* 
- * quoted-printable implementation was inspired by altermime http://www.altermime.org
- */
 #define _GNU_SOURCE
 
 #include "cmime_qp.h"
